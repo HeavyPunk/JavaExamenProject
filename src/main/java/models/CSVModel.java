0584bd1efb491bd4554a68335a36be48b4df7050
HeavyPunk <kirillkuchma8@gmail.com>
2021-12-12 12:24:28 +0500

    Added models

---
+++ src/main/java/models/CSVModel.java
@@ -0,0 +1,6 @@
+package models;
+
+public class CSVModel {
+    public String[] titles;
+    public String[][] data;
+}
---
+++ src/main/java/models/PassengerModel.java
@@ -0,0 +1,18 @@
+package models;
+
+import abstractions.Model;
+
+public class PassengerModel implements Model {
+    public int id;
+    public boolean survived;
+    public int pclass;
+    public String name;
+    public String sex;
+    public int age;
+    public int sibSp;
+    public int parch;
+    public String ticket;
+    public float fare;
+    public String cabin;
+    public String embarked;
+}
